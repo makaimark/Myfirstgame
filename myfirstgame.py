d2e3d750554ce50da9716137acbd00a0fbfdecca
makaimark <makaimark@gmail.com>
2016-05-18 12:40:36 +0200

    2.0

--- myfirstgame.py
+++ myfirstgame.py
@@ -7,16 +7,17 @@ def main(scr):
     curses.noecho()  #Disable default printing of inputs
     curses.curs_set(0) #hiding cursor visibility
     win = curses.newwin(curses.LINES, curses.COLS, 0, 0) # Init window object
-    win.border(5)
+    win.border(0)
 
-    key = win.getch()     # get the code of pressed key
+    list = str(win.getmaxyx())
+    win.addstr(0,0,list)
+
+    key = ""
 
     while key != 27:            # not Esc is pressed
         win.timeout(100)        # wait 0.1 sec
-
-
         win.refresh()
+        key = win.getch()     # get the code of pressed key
 
-    win.refresh()
 
 curses.wrapper(main)
