2e33fefa7484c6f9e704d1dd4db97240fbbe3298
makaimark <makaimark@gmail.com>
2016-05-19 16:23:30 +0200

    final version 1.0

--- fullgame.py
+++ fullgame.py
@@ -30,16 +30,23 @@ def main(scr):
             random_object_number = random.randint(0,4)  #help to choose a level1 object
             result = []
 
-        if score <= 0 and score <= 10:
+        if score >= 0 and score <= 10:
+            for index, value in enumerate(objects.level1[random_object_number]):  # responsible for the bomb object drawing
+                list2 = str(value)
+                win.addstr(index+1+y_coordinate,x_coordinate,list2)
             if (key == 97 and random_object_number == 0) or (key == 98 and random_object_number == 1) or (key == 99 and random_object_number == 2) or (key == 100 and random_object_number == 3) or (key == 101 and random_object_number == 4):
                 bomb_is_killed = True       # previous line : if the pressed key = the object's letter, kill the bomb
                 score += 1       # if the bomb killed, add one to the score
                 win.timeout(500)
-                key = ""
+                win.addstr(5,5,"Bomb is killed")
                 continue
             else:
                 bomb_is_killed = False
-        elif score <= 11 and score <= 20:
+
+        elif score >= 11 and score <= 20:
+            for index, value in enumerate(objects2.level2[random_object_number]):  # responsible for the bomb object drawing
+                list2 = str(value)
+                win.addstr(index+1+y_coordinate,x_coordinate,list2)
             if (key == 54 and random_object_number == 0) or (key == 57 and random_object_number == 1) or (key == 56 and random_object_number == 2) or (key == 51 and random_object_number == 3) or (key == 53 and random_object_number == 4):
                 bomb_is_killed = True       # previous line : if the pressed key = the object's letter, kill the bomb
                 score += 1       # if the bomb killed, add one to the score
@@ -48,7 +55,12 @@ def main(scr):
                 continue
             else:
                 bomb_is_killed = False
-        elif score <=21 and score <= 30:
+                key = ""
+        elif score >=21 and score <= 30:
+            for index, value in enumerate(objects3.level3[random_object_number]):  # responsible for the bomb object drawing
+                list2 = str(value)
+                win.addstr(index+1+y_coordinate,x_coordinate,list2)
+            win.addstr(10,10,str(result))
             if (len(result) == 3 and ((result[0] == 97 and result[1] == 98 and result[2] == 99 and random_object_number == 0) \
             or ( result[0] == 100 and result[1] == 101 and result[2] == 102 and random_object_number == 1) \
             or ( result[0] == 103 and result[1] == 104 and result[2] == 105 and random_object_number == 2) \
@@ -64,10 +76,12 @@ def main(scr):
                 bomb_is_killed = False
                 if key != -1:
                     result.append(key)
+        elif score >= 31:
+            win.addstr(12,40,"Victory!!!")
 
-        for index, value in enumerate(objects.level1[random_object_number]):  # responsible for the bomb object drawing
+        '''for index, value in enumerate(objects.level1[random_object_number]):  # responsible for the bomb object drawing
             list2 = str(value)
-            win.addstr(index+1+y_coordinate,x_coordinate,list2)
+            win.addstr(index+1+y_coordinate,x_coordinate,list2)'''
 
         y_coordinate =  y_coordinate + 1                # responsible for the y movement
         y = index + 1 + y_coordinate
@@ -76,6 +90,7 @@ def main(scr):
         win.timeout(500)        # wait 0,1 sec
 
         key = win.getch()
+        
 
 
 
