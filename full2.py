ef3fa7c896112205542d876469daacd362464c6b
makaimark <makaimark@gmail.com>
2016-06-30 20:27:31 +0200

    with more function

--- full2.py
+++ full2.py
@@ -14,10 +14,8 @@ def name_input():
     name1 = input("Write here your name: ")
     return name1
 
-name = name_input()
 
-
-def draw_menu(win):
+def draw_menu(win):         # draw the menu to the screen
     win.border(0)
     win.addstr(2, 30, str("Kill the Bombs"), curses.A_BOLD)
     win.addstr(3, 22, str("All rights reserved by codecool"))
@@ -27,9 +25,7 @@ def draw_menu(win):
     win.addstr(8, 3, str("Press SPACE to exit"))
 
 
-# def menu(key_menu):
-
-def main(scr):
+def init_playground():
     curses.noecho()  # Disable default printing of inputs
     curses.curs_set(0)  # hiding cursor visibility
     win = curses.newwin(curses.LINES, curses.COLS, 0, 0)  # Init window object
@@ -41,7 +37,35 @@ def main(scr):
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
     win.bkgd(' ', curses.color_pair(3))         # color the background
     win.refresh()
+    return win
 
+
+def refresh_screen(win, score_output, life):
+    win.clear()             # clear screen
+    win.border(0)           # draw border
+    win.addstr(1, 2, str("Your score:"))
+    win.addstr(2, 2, str(score_output))  # draw score
+    win.addstr(3, 2, " ❤ " * life, curses.color_pair(1))  # show your number of lifes
+
+
+def show_kill_effect(win):
+    win.addstr(5, 2, " ☠ ", curses.color_pair(1))     # Write a skull if bomb killed
+
+
+def create_new_bomb(win, y_coordinate, x_coordinate, result, bomb_is_killed, rand_char_level3):
+    if bomb_is_killed:
+        show_kill_effect(win)
+
+    y_coordinate = 0                # set y coordinate to 0
+    x_coordinate = random.randint(12, 75)  # random x coordinate
+    result = []  # this makes the result list empty again
+    bomb_is_killed = False
+    rand_char_level3 = []
+    return (y_coordinate, x_coordinate, result, bomb_is_killed, rand_char_level3)
+
+
+def main(scr):
+    win = init_playground()
     index = 0  # init index, what helps to draw the objects line by line
     key = ""
     y_coordinate = 0
@@ -54,11 +78,11 @@ def main(scr):
     score_output = 0
     life = 3
     key_menu = ""
-    operands = ['+', '-', '*', '/']
     level2_result = []
     rand_char_level3 = []
     rand_char_level1 = ""
     highscore = []
+    operands = ['+', '-', '*', '/']
 
     while True:
         draw_menu(win)
@@ -69,7 +93,7 @@ def main(scr):
             score = 0
         elif key_menu == 50:    # press 2 to stage two
             score = 11
-        elif key_menu == 51:    # press 2 to stage two
+        elif key_menu == 51:    # press 3 to stage 3
             score = 21
         elif key_menu == 32:    # press space to exit
             key = 32
@@ -77,28 +101,14 @@ def main(scr):
 
         life = 3
 
-        if key_menu == 49 or key_menu == 50 or key_menu == 51:
+        if key_menu == 49 or key_menu == 50 or key_menu == 51:      # if the player choose a valid stage, continue
 
             while key != 27:            # not Esc is pressed
-                win.clear()             # clear screen
-                win.border(0)           # draw border
-                win.addstr(1, 2, str("Your score:"))
-                win.addstr(2, 2, str(score_output))  # draw score
-                win.addstr(3, 2, " ❤ " * life, curses.color_pair(1))  # show your number of lifes
 
-                if bomb_is_killed or y_coordinate == 0 or y == 23:  # begining of the loop, make a random x coordinate
-                    if bomb_is_killed:
-                        win.addstr(5, 2, " ☠ ", curses.color_pair(1))     # Write a skull if bomb killed
-                        curses.beep()
-                        win.timeout(3000)
-                        win.refresh()
+                refresh_screen(win, score_output, life)
 
-                    y_coordinate = 0                # set y coordinate to 0
-                    x_coordinate = random.randint(12, 75)  # random x coordinate
-                    random_object_number = random.randint(0, 4)  # help to choose a level1 object
-                    result = []  # this makes the result list empty again
-                    bomb_is_killed = False
-                    rand_char_level3 = []
+                if bomb_is_killed or y_coordinate == 0 or y == 23:  # if the bomb killed or new bomb or not detonated
+                    y_coordinate, x_coordinate, result, bomb_is_killed, rand_char_level3 = create_new_bomb(win, y_coordinate, x_coordinate, result, bomb_is_killed, rand_char_level3)
 
                 if score >= 0 and score <= 10:  # level 1, between score 0-10
                     if y_coordinate == 0:
@@ -120,6 +130,7 @@ def main(scr):
                 elif score >= 11 and score <= 20:
                     if y_coordinate == 0:
                         result_operand = -1
+                        # if the result is between 0 and 9
                         while result_operand > 9 or result_operand < 0 or result_operand % 1:
                             rand_num1 = random.choice(list(string.digits))      # random number 1
                             rand_operand = random.choice(operands)      # choice from the operands
@@ -166,8 +177,9 @@ def main(scr):
                         list2 = str(value)
                         win.addstr(index+1+y_coordinate, x_coordinate, list2, curses.color_pair(2))
 
+                        # previous: if the pressed buttons are OK, kill the bomb
                     if (len(result) == 3 and result[0] == ord(rand_char_level3[0]) and result[1] == ord(rand_char_level3[1]) and result[2] == ord(rand_char_level3[2])):
-                        bomb_is_killed = True       # previous: if the pressed buttons are OK, kill the bomb
+                        bomb_is_killed = True
                         score += 1       # if the bomb killed, add one to the score
                         score_output += 1
                         win.timeout(250)
@@ -181,7 +193,7 @@ def main(scr):
 
                 if score >= 31:
                     win.clear()
-                    win.addstr(12, 40, "Victory!!! Press p to play a new game")  # press p to play a new game
+                    win.addstr(12, 40, "Victory!!! Press p to play a new game")
                     win.addstr(13, 40, "Press m to reach the menu")
 
                     with open('highscore.txt', mode='r') as f:  # write the name and the result to the txt
@@ -240,4 +252,8 @@ def main(scr):
 
                 if key == 27:       # if we got ESC, break
                     win.clear()
+
+
+name = name_input()         # here we are waiting for the gamer name
+
 curses.wrapper(main)
